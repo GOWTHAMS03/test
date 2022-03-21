079e3a9b70abfab7ba511f5da7ab5855472b52e2
GOWTHAMS03 <86407286+GOWTHAMS03@users.noreply.github.com>
2022-03-21 20:08:05 +0530

    Add files via upload

---
+++ alphabets.py
@@ -0,0 +1,14 @@
+for star in range(7):
+    print("* ",end =' ')
+print() 
+for row in range(7):
+    for star in range(7):
+        if star==0 or star==6 :
+            print("* ",end =' ')
+        else:
+            print("  ",end =' ')
+    print() 
+
+for star in range(7):
+    print("* ",end =' ')
+print() 
